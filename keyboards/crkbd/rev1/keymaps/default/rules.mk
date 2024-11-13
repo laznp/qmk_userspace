88061f714ae4d251a9b6087dcaab2242b3fe77e2
Lazuardi N Putra <me@laznp.id>
2024-11-13 18:17:50 +0700

    main update

---
+++ keyboards/crkbd/rev1/keymaps/default/rules.mk
@@ -0,0 +1,21 @@
+VIA_ENABLE          = no
+VIAL_ENABLE         = no
+LTO_ENABLE          = yes
+COMBO_ENABLE        = yes
+
+VIAL_INSECURE       = no
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no
+MOUSEKEY_ENABLE     = no
+OLED_ENABLE         = no
+OLED_DRIVER         = ssd1306
+QMK_SETTINGS        = no
+SPACE_CADET_ENABLE  = no
+GRAVE_ESC_ENABLE    = no
+MAGIC_ENABLE        = no
+MUSIC_ENABLE        = no
+KEY_OVERRIDE_ENABLE = no
+EXTRAKEY_ENABLE     = no
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
